a41f43ce0e5a397bffd6a1ff27ff1dad655f55f2
segfault <s9gf4ult@gmail.com>
2012-01-27 18:46:45 +0600

    db schema saved

--- db.sql
+++ db.sql
@@ -8,6 +8,69 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
+SET search_path = public, pg_catalog;
+
+ALTER TABLE ONLY public.user_parameter DROP CONSTRAINT user_parameter_user;
+ALTER TABLE ONLY public.session_parameter DROP CONSTRAINT session_parameter_session;
+ALTER TABLE ONLY public.project DROP CONSTRAINT project_status_id_fkey;
+ALTER TABLE ONLY public.project DROP CONSTRAINT project_ruleset_id_fkey;
+DROP INDEX public.fki_session_parameter_session;
+ALTER TABLE ONLY public.users DROP CONSTRAINT user_pkey;
+ALTER TABLE ONLY public.user_parameter DROP CONSTRAINT user_parameter_user_id_name_key;
+ALTER TABLE ONLY public.user_parameter DROP CONSTRAINT user_parameter_pkey;
+ALTER TABLE ONLY public.users DROP CONSTRAINT user_login_key;
+ALTER TABLE ONLY public.session DROP CONSTRAINT session_uuid_key;
+ALTER TABLE ONLY public.session DROP CONSTRAINT session_pkey;
+ALTER TABLE ONLY public.session_parameter DROP CONSTRAINT session_parameter_session_id_name_key;
+ALTER TABLE ONLY public.session_parameter DROP CONSTRAINT session_parameter_pkey;
+ALTER TABLE ONLY public.project_ruleset DROP CONSTRAINT ruleset_pkey;
+ALTER TABLE ONLY public.project_status DROP CONSTRAINT project_status_pkey;
+ALTER TABLE ONLY public.project_status DROP CONSTRAINT project_status_name_key;
+ALTER TABLE ONLY public.project_ruleset DROP CONSTRAINT project_ruleset_name_key;
+ALTER TABLE ONLY public.project DROP CONSTRAINT project_pkey;
+ALTER TABLE ONLY public.participant DROP CONSTRAINT participant_project_id_user_id_key;
+ALTER TABLE ONLY public.participant DROP CONSTRAINT participant_project_id_nm_key;
+ALTER TABLE ONLY public.participant DROP CONSTRAINT participant_pkey;
+ALTER TABLE public.users ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.user_parameter ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.session_parameter ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.session ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.project_status ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.project_ruleset ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.project ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.participant ALTER COLUMN id DROP DEFAULT;
+DROP SEQUENCE public.user_parameter_id_seq;
+DROP TABLE public.user_parameter;
+DROP SEQUENCE public.user_id_seq;
+DROP TABLE public.users;
+DROP SEQUENCE public.session_parameter_id_seq;
+DROP TABLE public.session_parameter;
+DROP SEQUENCE public.session_id_seq;
+DROP TABLE public.session;
+DROP SEQUENCE public.ruleset_id_seq;
+DROP SEQUENCE public.project_status_id_seq;
+DROP TABLE public.project_status;
+DROP TABLE public.project_ruleset;
+DROP SEQUENCE public.project_id_seq;
+DROP TABLE public.project;
+DROP SEQUENCE public.participant_id_seq;
+DROP TABLE public.participant;
+DROP EXTENSION plpgsql;
+DROP SCHEMA public;
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA public;
+
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
 --
@@ -29,13 +92,257 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: participant; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE participant (
+    id integer NOT NULL,
+    project_id integer NOT NULL,
+    is_initiator boolean DEFAULT false NOT NULL,
+    user_id integer,
+    nm character varying(50) NOT NULL,
+    descr text,
+    accept boolean DEFAULT false NOT NULL,
+    creation timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: COLUMN participant.is_initiator; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN participant.is_initiator IS 'the sign that this participant is initiator of the project';
+
+
+--
+-- Name: COLUMN participant.user_id; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN participant.user_id IS 'registered user if exists';
+
+
+--
+-- Name: COLUMN participant.nm; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN participant.nm IS 'nick of participant in current project';
+
+
+--
+-- Name: COLUMN participant.descr; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN participant.descr IS 'detailed description ';
+
+
+--
+-- Name: COLUMN participant.accept; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN participant.accept IS 'paticipant is accepted for project';
+
+
+--
+-- Name: participant_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE participant_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: participant_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE participant_id_seq OWNED BY participant.id;
+
+
+--
+-- Name: project; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE project (
+    id integer NOT NULL,
+    nm text NOT NULL,
+    descr text,
+    sharing boolean DEFAULT true NOT NULL,
+    begin timestamp with time zone,
+    "end" timestamp with time zone,
+    creation timestamp with time zone DEFAULT now() NOT NULL,
+    ruleset_id integer NOT NULL,
+    status_id integer NOT NULL
+);
+
+
+--
+-- Name: COLUMN project.nm; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN project.nm IS 'name of the project';
+
+
+--
+-- Name: COLUMN project.descr; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN project.descr IS 'description of the project';
+
+
+--
+-- Name: COLUMN project.sharing; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN project.sharing IS 'true if project is open';
+
+
+--
+-- Name: COLUMN project."end"; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN project."end" IS 'timestamp of the project''s final';
+
+
+--
+-- Name: COLUMN project.creation; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN project.creation IS 'timestamp of project creation';
+
+
+--
+-- Name: project_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE project_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: project_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE project_id_seq OWNED BY project.id;
+
+
+--
+-- Name: project_ruleset; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE project_ruleset (
+    id integer NOT NULL,
+    name character varying,
+    descr text
+);
+
+
+--
+-- Name: TABLE project_ruleset; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE project_ruleset IS 'ways to control the management of project';
+
+
+--
+-- Name: COLUMN project_ruleset.name; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN project_ruleset.name IS 'name of the way to control the project';
+
+
+--
+-- Name: COLUMN project_ruleset.descr; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN project_ruleset.descr IS 'description of the way to control';
+
+
+--
+-- Name: project_status; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE project_status (
+    id integer NOT NULL,
+    name character varying(50) NOT NULL,
+    descr text
+);
+
+
+--
+-- Name: TABLE project_status; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE project_status IS 'status of the current project';
+
+
+--
+-- Name: COLUMN project_status.name; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN project_status.name IS 'name of status';
+
+
+--
+-- Name: COLUMN project_status.descr; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN project_status.descr IS 'detailed description of status';
+
+
+--
+-- Name: project_status_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE project_status_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: project_status_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE project_status_id_seq OWNED BY project_status.id;
+
+
+--
+-- Name: ruleset_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE ruleset_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: ruleset_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE ruleset_id_seq OWNED BY project_ruleset.id;
+
+
+--
 -- Name: session; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE session (
     id integer NOT NULL,
     uuid character varying(45) NOT NULL,
-    expiration_date timestamp with time zone
+    expiration_date timestamp with time zone,
+    creation_date timestamp with time zone DEFAULT now() NOT NULL
 );
 
 
@@ -91,15 +398,17 @@ ALTER SEQUENCE session_parameter_id_seq OWNED BY session_parameter.id;
 
 
 --
--- Name: user; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+-- Name: users; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
-CREATE TABLE "user" (
+CREATE TABLE users (
     id integer NOT NULL,
     name character varying(50),
     login character varying(50) NOT NULL,
     password character varying,
-    email character varying(120)
+    email character varying(120),
+    creation_date timestamp with time zone DEFAULT now() NOT NULL,
+    deleted boolean DEFAULT false NOT NULL
 );
 
 
@@ -119,7 +428,7 @@ CREATE SEQUENCE user_id_seq
 -- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
-ALTER SEQUENCE user_id_seq OWNED BY "user".id;
+ALTER SEQUENCE user_id_seq OWNED BY users.id;
 
 
 --
@@ -157,6 +466,34 @@ ALTER SEQUENCE user_parameter_id_seq OWNED BY user_parameter.id;
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
+ALTER TABLE participant ALTER COLUMN id SET DEFAULT nextval('participant_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE project ALTER COLUMN id SET DEFAULT nextval('project_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE project_ruleset ALTER COLUMN id SET DEFAULT nextval('ruleset_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE project_status ALTER COLUMN id SET DEFAULT nextval('project_status_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
 ALTER TABLE session ALTER COLUMN id SET DEFAULT nextval('session_id_seq'::regclass);
 
 
@@ -171,14 +508,78 @@ ALTER TABLE session_parameter ALTER COLUMN id SET DEFAULT nextval('session_param
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE "user" ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regclass);
+ALTER TABLE user_parameter ALTER COLUMN id SET DEFAULT nextval('user_parameter_id_seq'::regclass);
 
 
 --
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE user_parameter ALTER COLUMN id SET DEFAULT nextval('user_parameter_id_seq'::regclass);
+ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regclass);
+
+
+--
+-- Name: participant_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY participant
+    ADD CONSTRAINT participant_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: participant_project_id_nm_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY participant
+    ADD CONSTRAINT participant_project_id_nm_key UNIQUE (project_id, nm);
+
+
+--
+-- Name: participant_project_id_user_id_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY participant
+    ADD CONSTRAINT participant_project_id_user_id_key UNIQUE (project_id, user_id);
+
+
+--
+-- Name: project_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY project
+    ADD CONSTRAINT project_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: project_ruleset_name_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY project_ruleset
+    ADD CONSTRAINT project_ruleset_name_key UNIQUE (name);
+
+
+--
+-- Name: project_status_name_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY project_status
+    ADD CONSTRAINT project_status_name_key UNIQUE (name);
+
+
+--
+-- Name: project_status_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY project_status
+    ADD CONSTRAINT project_status_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: ruleset_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY project_ruleset
+    ADD CONSTRAINT ruleset_pkey PRIMARY KEY (id);
 
 
 --
@@ -217,7 +618,7 @@ ALTER TABLE ONLY session
 -- Name: user_login_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY "user"
+ALTER TABLE ONLY users
     ADD CONSTRAINT user_login_key UNIQUE (login);
 
 
@@ -241,7 +642,7 @@ ALTER TABLE ONLY user_parameter
 -- Name: user_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
-ALTER TABLE ONLY "user"
+ALTER TABLE ONLY users
     ADD CONSTRAINT user_pkey PRIMARY KEY (id);
 
 
@@ -250,6 +651,22 @@ ALTER TABLE ONLY "user"
 --
 
 CREATE INDEX fki_session_parameter_session ON session_parameter USING btree (session_id);
+
+
+--
+-- Name: project_ruleset_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY project
+    ADD CONSTRAINT project_ruleset_id_fkey FOREIGN KEY (ruleset_id) REFERENCES project_ruleset(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+
+--
+-- Name: project_status_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY project
+    ADD CONSTRAINT project_status_id_fkey FOREIGN KEY (status_id) REFERENCES project_status(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 
 --
@@ -265,17 +682,7 @@ ALTER TABLE ONLY session_parameter
 --
 
 ALTER TABLE ONLY user_parameter
-    ADD CONSTRAINT user_parameter_user FOREIGN KEY (user_id) REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- Name: public; Type: ACL; Schema: -; Owner: -
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
+    ADD CONSTRAINT user_parameter_user FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
